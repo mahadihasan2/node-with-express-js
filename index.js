ee37d740ee5f3646e89937ed779cf4a8857087c9
mahadihasan2 <mahadihasan125643@gmail.com>
2022-05-10 03:11:04 +0600

    cros install

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Look mama i can do node now ! I am Happy! to say!to Hello!to come');
